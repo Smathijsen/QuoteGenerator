a7c1a928bb6c704107570833fd9922da00a7a99e
Scarlett Mathijsen <1204823mathijsen@zuyd.nl>
2019-09-16 23:19:50 +0200

    added quote to be added in social media message

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,51 +1,42 @@
 <template>
-    <div>
-        <div class ="quote">
-            <slot name ="quote"></slot>
-        </div>
-        <div class ="author">
-            <slot name ="author"></slot>
-        </div>
+  <div>
+    <div class="quote">
+      <!-- {{results.data.quote}} -->
+      <slot name="quote"></slot>
     </div>
+    <div class="author">
+      <!-- {{results.data.author}} -->
+      <slot name="author"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {  
-
-}
+export default {
+  //Function to get random quote from API
+  //   mounted() {
+  //     axios
+  //       .get("http://quotes.stormconsultancy.co.uk/quotes/random.json")
+  //       .then(response => (this.results = response));
+  //   },
+};
 </script>
 
 <style scoped>
+div {
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
 
-    div {
-        /* border: 1px solid #ccc;
-        box-shadow: 1px 1px 2px white; */
-        padding: 30px;
-        margin: 30px auto;
-        text-align: center;
-        /* background-color: #22264b; */
-        
-    }
+.quote {
+  font-style: italic;
+  color: white;
+  font-size: 30px;
+}
 
-    h1 {
-        color:white;
-        /* color: #BB86FC; */
-        /**
-        SECONDARY COLOR:
-
-        color: #03DAC5       
-        
-         */
-    }
-/* .content */
-    .quote {
-        font-style: italic;
-        color: white;
-        font-size: 30px;
-    }
-
-    .author {
-        color:white;
-        font-size:30px;
-    }
+.author {
+  color: white;
+  font-size: 30px;
+}
 </style>
